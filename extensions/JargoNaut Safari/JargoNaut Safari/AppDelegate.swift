a5def9d0bbd95ab4784c6793109b8a62395f7ee8
CuWilliams <142925529+CuWilliams@users.noreply.github.com>
2025-10-07 13:14:12 -0230

    Add Safari extension - JargoNaut now works on Safari for Mac

---
+++ extensions/JargoNaut Safari/JargoNaut Safari/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  JargoNaut Safari
+//
+//  Created by Curtis Williams on 2025-10-07.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
